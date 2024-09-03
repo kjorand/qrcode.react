107f36fad8204b7421fbfc0b061965d0d9bdc140
kjorand <25977492+kjorand@users.noreply.github.com>
2024-09-03 16:53:30 +0200

    Correct typo in index.tsx
    Typo in "foreground"

--- src/index.tsx
+++ src/index.tsx
@@ -91,7 +91,7 @@ type QRProps = {
    */
   bgColor?: string;
   /**
-   * The foregtound color used to render the QR Code.
+   * The foreground color used to render the QR Code.
    * @see https://developer.mozilla.org/en-US/docs/Web/CSS/color_value
    * @defaultValue #000000
    */
